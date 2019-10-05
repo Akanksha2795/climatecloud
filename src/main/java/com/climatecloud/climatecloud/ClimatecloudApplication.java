693b38784ded67f05ed6daedbabdb4aa7ff47cf8
Akanksha2795 <kapureakanksha21@gmail.com>
2019-10-05 11:35:53 -0700

    jhjh

---
+++ src/main/java/com/climatecloud/climatecloud/ClimatecloudApplication.java
@@ -0,0 +1,13 @@
+package com.climatecloud.climatecloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimatecloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimatecloudApplication.class, args);
+	}
+
+}
